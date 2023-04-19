9ed70d2f2baf7356c42c982c34d7041bed9c2f5a
CamilaAchuri <camilaachury.4@gmail.com>
2023-04-19 16:15:53 -0300

    DSKTCH-94 - preparacion de tooltip con dsdataprep

---
+++ R/compat-utils.R
@@ -0,0 +1,88 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr select mutate summarise
+#' @importFrom dstools %||%
+## usethis namespace: end
+NULL
+
+
+#' @keywords internal
+frtype_viz <- function(var_gnm = NULL,
+                       var_gcd = NULL,
+                       var_gln = NULL,
+                       var_glt = NULL,
+                       var_num = NULL) {
+
+  frtype <- NULL
+  if (!is.null(var_gnm)) frtype <- paste0(rep("Gnm", length(var_gnm)), collapse = "")
+  if (!is.null(var_gcd)) frtype <- paste0(frtype, paste0(rep("Gcd", length(var_gcd)), collapse = ""), collapse = "")
+  if (!is.null(var_gln)) frtype <- paste0(frtype, paste0(rep("Gln", length(var_gln)), collapse = ""), collapse = "")
+  if (!is.null(var_glt)) frtype <- paste0(frtype, paste0(rep("Glt", length(var_glt)), collapse = ""), collapse = "")
+  if (!is.null(var_num)) frtype <- paste0(frtype, paste0(rep("Num", length(var_num)), collapse = ""), collapse = "")
+
+  frtype
+}
+
+#' @keywords internal
+data_map_draw <- function(data = NULL,
+                          dic = NULL,
+                          var_geo = NULL,
+                          var_num = NULL,
+                          opts = NULL) {
+
+  map_name <- opts$map_name
+  if(is.null(map_name)) stop("No map name provided, see available_maps()")
+
+  tj <- geodato::gd_tj(map_name)
+
+  if(!is.null(data)){
+
+    vars <- c(var_geo, var_num)
+    data <- data |>
+      dplyr::select(vars, dplyr::everything())
+
+    if (length(var_geo) == 1) {
+      col <- geodato::parse_col(data, var_geo)
+    }
+
+
+    if (!"..labels" %in% names(data)) {
+      data$label <- dsdataprep::prep_tooltip(data = data,
+                                             tooltip = opts$tooltip_template,
+                                             new_labels = NULL,
+                                             engine = "html",
+                                             as_df = FALSE,
+                                             na_row_default_column = NULL,
+                                             na_row_default_value = NULL,
+                                             na_label = opts$na_label,
+                                             format_num = opts$format_sample_num,
+                                             opts_format_num = list(prefix = opts$prefix_num,
+                                                                    suffix = opts$suffix_num,
+                                                                    si_prefix = opts$si_prefix),
+                                             format_cat = opts$format_sample_cat,
+                                             format_date = opts$format_sample_dat)
+    }
+
+    data_join <- geodato::gd_match(data, map_name)
+
+    dgeo <- tj |>
+      dplyr::left_join(data_join, by = c(id = "..gd_id", name = "..gd_name"))
+    dgeo <- dgeo |>
+      dplyr::mutate(label = ifelse(is.na(label), name, label) |>
+                      lapply(htmltools::HTML))
+
+  } else{
+    dgeo <- tj
+  }
+
+
+
+  list(
+    var_geo = var_geo,
+    map_data = dgeo
+  )
+
+
+}
