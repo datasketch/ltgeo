353cb8495d29a6658d0dffb316d6a388c37ff9a4
CamilaAchuri <camilaachury.4@gmail.com>
2023-04-27 16:06:03 -0300

    funcion de grafico de puntos

--- R/lt-utils.R
+++ R/lt-utils.R
@@ -22,3 +22,39 @@ ltg_choropleth <- function(map, opts) {
       highlightOptions = opts$highlight_options
     )
 }
+
+#' @keywords internal
+ltg_circles <- function(map, opts) {
+
+  cluster_add <- NULL
+  if (opts$cluster_add) {
+    cluster_add <- do.call("markerClusterOptions", opts$cluster_opts)
+  }
+
+  map |>
+    addCircleMarkers(
+      data = opts$extra_data %||% getMapData(map),
+      lng = ~lon,
+      lat = ~lat,
+      radius = opts$map_radius %||% ~scales::rescale(..domain, 3, 15),
+      layerId = opts$layer_id,
+      group = opts$group,
+      stroke = opts$stroke,
+      color = opts$map_circle_color,
+      weight = opts$map_circle_weight,
+      opacity = opts$map_circle_opacity,
+      fill = opts$fill,
+      fillColor = opts$map_circle_color %||% ~opts$pal(..domain),
+      fillOpacity =  opts$map_circle_fill_opactity,
+      dashArray =  opts$map_circle_dashArray,
+      popup = opts$map_circle_pupup,
+      popupOptions = opts$map_circle_popupOptions,
+      label = ~label,
+      labelOptions = opts$map_circle_labelOptions,
+      clusterOptions = cluster_add,
+      clusterId = opts$map_cluster_id
+    )
+
+
+}
+
