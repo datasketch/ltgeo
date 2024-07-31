62381c470881be9944b35339c6f3516bb7813008
CamilaAchuri <camilaachury.4@gmail.com>
2024-07-31 18:34:47 -0300

    add cluster option

--- R/lt-bubbles.R
+++ R/lt-bubbles.R
@@ -10,6 +10,7 @@ lt_bubbles <- function(data = NULL,
   base_map <- lt_choropleth(...)
   if (is.null(data)) return(base_map)
   opts_data <- dsopts_merge(..., categories = "dataprep")
+  general_opts <- dsopts_merge(..., categories = "map")
   opts_bubble <- dsopts_merge(..., categories = "mapbubble")
   bubble_min <- opts_bubble$bubble_min %||% 3
   bubble_max <- opts_bubble$bubble_min %||% 15
@@ -19,6 +20,12 @@ lt_bubbles <- function(data = NULL,
                                 var_gln = var_gln, var_glt = var_glt,
                                 var_num = var_num,
                                 map_name = opts_data$map_name, opts = opts_data)
+  add_cluster <- NULL
+  if (opts_bubble$map_bubble_cluster) {
+    add_cluster <- do.call(eval(parse(text="leaflet::markerClusterOptions")),
+                           opts_bubble$map_bubble_cluster_params %||% list(showCoverageOnHover = FALSE))
+  }
+
   lt <- base_map |>
     leaflet::addCircleMarkers(
       data = data_viz,
@@ -29,17 +36,17 @@ lt_bubbles <- function(data = NULL,
       # group = opts$group,
       # stroke = opts$stroke,
       # color = opts$map_circle_color,
-      # weight = opts$map_circle_weight,
-      #opacity = opts_bubble$bubble_opacity,
+      weight = general_opts$border_width,
+      opacity = general_opts$bubble_opacity,
       # fill = opts$fill,
       # fillColor = opts$map_circle_color %||% ~opts$pal(..domain),
-      #fillOpacity =  opts_bubble$bubble_opacity,
+      fillOpacity =  general_opts$map_opacity %||% 0.8,
       # dashArray =  opts$map_circle_dashArray,
       # popup = opts$popup,
       # popupOptions = opts$popup_options,
-      label = ~..labels#,
+      label = ~..labels,
       # labelOptions = opts$label_options,
-      # clusterOptions = cluster_add,
+      clusterOptions = add_cluster#,
       # clusterId = opts$map_cluster_id
     )
   lt
--- tests/testthat/test-bubbles.R
+++ tests/testthat/test-bubbles.R
@@ -6,4 +6,32 @@ test_that("Bubble map", {
   )
   lt_bubbles(data = data,  var_gln = "Longitud",
              var_glt = "Latitud")
+
+  # Añadir agrupación
+  lt_bubbles(data = data,  var_gln = "Longitud",
+             var_glt = "Latitud", map_bubble_cluster = TRUE)
+
+  # Personalizar agrupación
+  # aca se puede ajustar:
+  # showCoverageOnHover: Cuando se pasa el cursor sobre un grupo de marcadores, muestra la cobertura del área que representa ese grupo.
+  # zoomToBoundsOnClick: Al hacer clic en un grupo de marcadores, el mapa se acercará para mostrar todos los marcadores individuales de ese grupo
+  # spiderfyOnMaxZoom: Si el usuario hace clic en un grupo de marcadores cuando el mapa está en su nivel de zoom máximo, los marcadores se "desplegarán" (spiderfy) para mostrarlos individualmente.
+  # removeOutsideVisibleBounds: Elimina los grupos de marcadores que están fuera de los límites visibles del mapa para mejorar el rendimiento.
+  # animateAddingMarkers: Controla si los nuevos marcadores se añaden con una animación.
+  # disableClusteringAtZoom: Desactiva la agrupación de marcadores a un nivel de zoom especificado.
+  # maxClusterRadius: Define el radio máximo en píxeles de un grupo de marcadores. Los marcadores que están más lejos no se agruparán
+  # spiderLegPolylineOptions: markerClusterOptions(spiderLegPolylineOptions = list(weight = 1.5, color = "#FF0000"))
+  # iconCreateFunction: Una función JavaScript que permite personalizar la apariencia del ícono del grupo de marcadores.
+  icon_create_function <- JS("
+  function(cluster) {
+    return L.divIcon({
+      html: '<div style=\"display: flex; justify-content: center; align-items: center; width: 40px; height: 40px; border-radius: 50%; background: rgba(255, 0, 0, 0.5);\"><span>' + cluster.getChildCount() + '</span></div>',
+      className: 'marker-cluster',
+      iconSize: L.point(40, 40)
+    });
+  }
+")
+  lt_bubbles(data = data,  var_gln = "Longitud",
+             var_glt = "Latitud", map_bubble_cluster = TRUE,
+             map_bubble_cluster_params = list(iconCreateFunction = icon_create_function))
 })
