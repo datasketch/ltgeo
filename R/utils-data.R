76ceae4bbf299b274f3977a62311667704e956d8
CamilaAchuri <camilaachury.4@gmail.com>
2024-07-30 21:46:10 -0300

    add data prep to plot bubbles map

--- R/utils-data.R
+++ R/utils-data.R
@@ -49,6 +49,75 @@ data_prep <- function(data = NULL,
 }
 
 
+data_prep_bubbles <- function(data = NULL,
+                              dic = NULL,
+                              var_cat = NULL,
+                              var_gln = NULL,
+                              var_glt = NULL,
+                              var_num = NULL,
+                              conmap = NULL,
+                              map_name = NULL, ...) {
+  dgeo <- NULL
+  no_conmap <- is.null(conmap)
+  conmap <- geotable::gt_con(conmap)
+
+  sf <- geotable::gt_sf(map_name, con = conmap) |>
+    geotable::rename_dotdot()
+
+  if (is.null(data)) {
+    dgeo <- sf |> mutate(..labels = ..gt_name)
+    dgeo$lon <- 0
+    dgeo$lon <- lat
+  } else {
+    data <- data |> rename("lat" = {{var_gln}}, "lon" = {{var_glt}})
+    opts <- dsopts_merge(..., categories = "dataprep")
+    if (is.null(var_num)) {
+      agg_num <- "count"
+    } else {
+      agg_num <- opts$agg %||% "sum"
+    }
+    data_viz <- dsdatawiz:::aggregate(
+      data = data,
+      group_vars = c(var_cat, "lat", "lon"),
+      var_num_to_agg = var_num,
+      agg = agg_num, agg_na_rm = TRUE,
+      tooltip_add_unique_cats = opts$tooltip_add_unique_cats
+    )
+
+    if (!is.null(dic)) {
+      dic$id[dic$id == var_gln] <- "lat"
+      dic$id[dic$id == var_glt] <- "lon"
+
+      if (is.null(var_num)) {
+        var_num <- "conteo"
+        dic <- bind_rows(dic,
+                         data.frame(id = "conteo", label = "Conteo", hdtype = "Num"))
+      }
+    }
+
+
+    ht <- hdtable(data_viz)
+    data <- ht$data
+    dic_d <- dic %||% ht$dic
+    if (is.null(dic)) {
+      dic_d$label[dic_d$id == "lat"] <- var_gln
+      dic_d$label[dic_d$id == "lon"] <- var_glt
+    }
+
+    dgeo <- aggregate_data(data = data,
+                           dic = dic_d,
+                           group_vars = NULL,
+                           var_num_to_agg = var_num, ...)
+  }
+
+  list(
+    data_polygon = sf,
+    data_geo = dgeo
+  )
+}
+
+
+
 default_var_group <- function(dic = NULL) {
   available_hdtypes <- c("Gnm", "Gcd", "Cat")
 
@@ -61,4 +130,9 @@ default_var_group <- function(dic = NULL) {
   var_group
 }
 
-
+default_var_coor <- function(dic = NULL) {
+  dic <- dic |> filter(hdtype %in% "Num")
+  if (nrow(dic) == 0) return()
+  list(glt = dic$id[1],
+       gln = dic$id[2])
+}
--- tests/testthat/test-data_prep.R
+++ tests/testthat/test-data_prep.R
@@ -1,6 +1,6 @@
 test_that("data_prep", {
 
-  # WITH DATA
+  # Gnm o Gcd
   data <- data.frame(
     name = c("BOGOTA, D.C.", "CAUCA", "ANTIOQUIA", "MAGDALENA", "MAGDALENA"),
     population = c(8000000, 1500000, 6500000, 1300000, 32423)
@@ -9,12 +9,33 @@ test_that("data_prep", {
   data_geo <- data_prep(data,
                         var_geo = "name",
                         var_num = "population",
-                        opts = list(map_name = "col_departments")
-                        )
+                        map_name = "col_departments"
+  )
 
 
+  # Gln y Glt
+  data <- data.frame(
+    Latitud = runif(30, min = -90, max = 90),
+    Longitud = runif(30, min = -180, max = 180)
+  )
 
+  data_coor <- data_prep_bubbles(data,
+                                var_gln = "Longitud",
+                                var_glt = "Latitud",
+                                map_name = "col_departments")
+  data_coor <- data_coor$data_geo
 
+  data <- data.frame(
+    Latitud = runif(30, min = -90, max = 90),
+    Longitud = runif(30, min = -180, max = 180),
+    letras = c(rep("a", 10), rep("b", 20))
+  )
 
+  data_coor <- data_prep_bubbles(data,
+                                var_gln = "Longitud",
+                                var_glt = "Latitud",
+                                map_name = "col_departments",
+                                tooltip_add_unique_cats = "letras")
+  data_coor <- data_coor$data_geo
 
 })
