b89297c2cd25ddda050be8e961edbd452f2a3c1c
CamilaAchuri <camilaachury.4@gmail.com>
2024-07-31 00:36:54 -0300

    add bubble map skeleton function

--- tests/testthat/test-data_prep.R
+++ tests/testthat/test-data_prep.R
@@ -19,23 +19,20 @@ test_that("data_prep", {
     Longitud = runif(30, min = -180, max = 180)
   )
 
-  data_coor <- data_prep_bubbles(data,
+  data_geo <- data_prep_bubbles(data,
                                 var_gln = "Longitud",
                                 var_glt = "Latitud",
                                 map_name = "col_departments")
-  data_coor <- data_coor$data_geo
 
   data <- data.frame(
-    Latitud = runif(30, min = -90, max = 90),
-    Longitud = runif(30, min = -180, max = 180),
-    letras = c(rep("a", 10), rep("b", 20))
+    name = c("BOGOTA, D.C.", "CAUCA", "ANTIOQUIA", "MAGDALENA", "MAGDALENA"),
+    population = c(8000000, 1500000, 6500000, 1300000, 32423)
   )
 
-  data_coor <- data_prep_bubbles(data,
-                                var_gln = "Longitud",
-                                var_glt = "Latitud",
-                                map_name = "col_departments",
-                                tooltip_add_unique_cats = "letras")
-  data_coor <- data_coor$data_geo
+  data_geo <- data_prep_bubbles(data,
+                                var_geo = "name",
+                                map_name = "col_departments")
+
+
 
 })
