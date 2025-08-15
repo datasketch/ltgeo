ddb662c861a769a006808fe5e08c961dc0808d68
CamilaAchuri <camilaachury.4@gmail.com>
2025-08-15 14:34:31 -0300

    Update utils-background.R

--- R/utils-background.R
+++ R/utils-background.R
@@ -1,25 +1,31 @@
 #' @keywords internal
 lt_tiles <- function(lt, opts) {
+  # Si no se especifica proveedor de mapas, solo se aplica el color de fondo
   if (is.null(opts$map_provider_tile)) {
-    lt <- lt |>
-      leaflet.extras::setMapWidgetStyle(list(background = opts$background_color))
-  } else {
-    switch(opts$map_provider_tile,
-           "leaflet" = lt <- lt |> leaflet::addProviderTiles(opts$map_tiles),
-           "esri" = {
-             lt <- lt |> leaflet.esri::addEsriBasemapLayer(leaflet.esri::esriBasemapLayers[[opts$map_tiles_esri]])
-             if (!is.null(opts$map_extra_layout)) {
-               lt <- lt |>
-                 leaflet.esri::addEsriFeatureLayer(
-                   url = opts$map_extra_layout)
-             }
-           },
-           {
-             lt <- lt |> leaflet::addTiles(urlTemplate = opts$map_extra_layout)
-           }
-    )
+    return(lt |> leaflet.extras::setMapWidgetStyle(list(background = opts$background_color)))
   }
 
-  lt
-}
+  # Si el proveedor es "leaflet"
+  if (opts$map_provider_tile == "leaflet") {
+    if (is.null(opts$map_tiles) || opts$map_tiles == "") {
+      lt <- lt |> leaflet.extras::setMapWidgetStyle(list(background = opts$background_color))
+    } else {
+      lt <- lt |> leaflet::addProviderTiles(opts$map_tiles)
+    }
 
+  # Si el proveedor es "esri"
+  } else if (opts$map_provider_tile == "esri") {
+    esri_layer <- leaflet.esri::esriBasemapLayers[[opts$map_tiles_esri]]
+    lt <- lt |> leaflet.esri::addEsriBasemapLayer(esri_layer)
+
+    if (!is.null(opts$map_extra_layout)) {
+      lt <- lt |> leaflet.esri::addEsriFeatureLayer(url = opts$map_extra_layout)
+    }
+
+  # Otro proveedor (personalizado)
+  } else {
+    lt <- lt |> leaflet::addTiles(urlTemplate = opts$map_extra_layout)
+  }
+
+  return(lt)
+}
